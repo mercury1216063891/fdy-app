4fa5cc619426cf0ba12c087be46768b65dd532f4
mercury <132160976+mercury1216063891@users.noreply.github.com>
2024-08-01 17:21:10 +0800

    Update 知识库问答.py

--- 知识库问答.py
+++ 知识库问答.py
@@ -96,7 +96,7 @@ def send_message():
     })
     # print(type(payload), payload)
     headers = {'Content-Type': 'application/json'}
-    url = "http://localhost:5000/api-dev/qa/get_answer"
+    url = "http://zklx.xtu.vip.cpolar.top/api-dev/qa/get_answer"
     response = requests.post(url, data=payload, headers=headers)
     # print(response, type(response))
     return response.text
