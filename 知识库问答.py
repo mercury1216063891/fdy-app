480fbb35a14772c7b66d6b7e404eaf9ec885c6a4
mercury <132160976+mercury1216063891@users.noreply.github.com>
2024-08-01 17:10:39 +0800

    Update 知识库问答.py

--- 知识库问答.py
+++ 知识库问答.py
@@ -1,53 +1,147 @@
 import streamlit as st
-from openai import OpenAI
+from PIL import Image
+import base64
+import requests
+import json
+from voice_toolkit import voice_toolkit
 
-# Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+icon_path = "images/院徽.ico"
+
+ICON = Image.open(icon_path)
+with open(icon_path, "rb") as img_file:
+    ICON_base64 = base64.b64encode(img_file.read()).decode()
+
+st.set_page_config(
+    page_title="智课灵犀-基于知识校园生活问答",
+    layout="centered",
+    page_icon=ICON,
 )
-
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
-
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+with st.sidebar:
+    icon_text = f"""
+        <div class="icon-text-container" style="text-align: center;">
+            <img src='data:image/png;base64,{ICON_base64}' alt='icon' style='width: 70px; height: 70px; margin: 0 auto; display: block;'>
+            <span style='font-size: 24px;'>课程助手--智课灵犀</span>
+        </div>
+        """
+    st.markdown(
+        icon_text,
+        unsafe_allow_html=True,
     )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
 
-    if uploaded_file and question:
+st.sidebar.title('输入')
+option2 = st.sidebar.selectbox('方式', ['键盘', '语音'])
 
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
+# 添加滑动条
+if "n_results" not in st.session_state:
+    st.session_state["n_results"] = 3
+if "max_new_tokens" not in st.session_state:
+    st.session_state["max_new_tokens"] = 500
+    st.session_state["top_p"] = 0.9
+    st.session_state["temperature"] = 0.1
+    st.session_state["repetition_penalty"] = 1.0
+st.sidebar.title('参数')
+with st.sidebar.expander("文本生成"):
+    parameter_5 = st.slider('n_results', min_value=1, max_value=5, value=st.session_state.n_results, step=1)
+    parameter_1 = st.slider('max_new_tokens', min_value=50, max_value=1000,
+                            value=st.session_state.max_new_tokens, step=50)
+    parameter_2 = st.slider('top_p', min_value=0.5, max_value=0.95, value=st.session_state.top_p, step=0.01)
+    parameter_3 = st.slider('temperature', min_value=0.1, max_value=5.0, value=st.session_state.temperature,
+                            step=0.1)
+    parameter_4 = st.slider('repetition_penalty', min_value=0.5, max_value=5.0,
+                            value=st.session_state.repetition_penalty, step=0.1)
 
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
+    st.session_state["n_results"] = parameter_5
+    st.session_state["max_new_tokens"] = parameter_1
+    st.session_state["top_p"] = parameter_2
+    st.session_state["temperature"] = parameter_3
+    st.session_state["repetition_penalty"] = parameter_4
+
+st.title("📚 智课灵犀")
+st.caption("🌈 基于校园生活知识库来进行问答")
+
+
+# 状态
+if "chat_type" not in st.session_state or st.session_state["chat_type"] == "chat":
+    st.session_state["chat_type"] = "chat_rag"
+
+if "is_recording" not in st.session_state:
+    st.session_state.is_recording = False
+
+if "user_input_area" not in st.session_state:
+    st.session_state.user_input_area = ""
+
+if "user_voice_value" not in st.session_state:
+    st.session_state.user_voice_value = ""
+
+if "voice_flag" not in st.session_state:
+    st.session_state["voice_flag"] = ""
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "message": "你好，我是湘潭大学课程知识答疑小助手“智课灵犀”。"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["message"])
+
+
+def send_message():
+    payload = json.dumps({
+        "chat_type": st.session_state.chat_type,
+        "messages": st.session_state.messages,
+        "max_new_tokens": st.session_state.max_new_tokens,
+        "top_p": st.session_state.top_p,
+        "temperature": st.session_state.temperature,
+        "repetition_penalty": st.session_state.repetition_penalty,
+        "n_results": st.session_state.n_results,
+    })
+    # print(type(payload), payload)
+    headers = {'Content-Type': 'application/json'}
+    url = "http://localhost:5000/api-dev/qa/get_answer"
+    response = requests.post(url, data=payload, headers=headers)
+    # print(response, type(response))
+    return response.text
+
+
+
+if option2 == "键盘":
+    if prompt := st.chat_input(placeholder="输入..."):
+        st.session_state.messages.append({"role": "user", "message": prompt})
+        st.chat_message("user").write(prompt)
+        answer = send_message()
+        st.session_state.messages.append({"role": "assistant", "message": answer})
+        st.chat_message("assistant").write(answer)
+        print(st.session_state)
+
+elif option2 == "语音":
+    # 文本输入表单
+    with st.form("input_form", clear_on_submit=True):
+        prompt = st.text_area(
+            "**输入：**",
+            key="user_input_area",
+            value=st.session_state.user_voice_value,
+            help="在此输入文本或通过语音输入。"
         )
+        submitted = st.form_submit_button("确认提交")
 
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+    # 处理提交
+    if submitted:
+        st.session_state.messages.append({"role": "user", "message": prompt})
+        st.chat_message("user").write(prompt)
+        answer = send_message()
+        st.session_state.messages.append({"role": "assistant", "message": answer})
+        st.chat_message("assistant").write(answer)
+
+        st.session_state.user_voice_value = ""
+        st.rerun()
+    # 语音输入
+    vocie_result = voice_toolkit()
+    # vocie_result会保存最后一次结果
+    if (
+            vocie_result and vocie_result["voice_result"]["flag"] == "interim"
+    ) or st.session_state["voice_flag"] == "interim":
+        st.session_state["voice_flag"] = "interim"
+        st.session_state["user_voice_value"] = vocie_result["voice_result"]["value"]
+        if vocie_result["voice_result"]["flag"] == "final":
+            st.session_state["voice_flag"] = "final"
+            st.rerun()
+
